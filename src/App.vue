4c0858296f56b9952d07d8d9ff1a6129131defeb
Ujjen Baniya <ujjenbaniya@Egens-MacBook-Pro.local>
2024-04-16 11:31:05 -0500

    feat: create new component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WelcomeComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeComponent from './components/WelcomeComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WelcomeComponent
   }
 }
 </script>
---
+++ src/components/WelcomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h2>Welcome to this repo. Here we create and test Github workflow for release.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeComponent',
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
